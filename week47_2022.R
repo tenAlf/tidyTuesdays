f0b30ccac107a734c1d53fb3e71cb2342a3d27b2
Alfred Jankowski <92812423+tenAlf@users.noreply.github.com>
2022-11-22 19:15:21 +0100

    Initial commit

---
+++ week47_2022.R
@@ -0,0 +1,17 @@
+# Setup -------------------------------------------------------------------
+
+# Packages
+library(tidyverse)
+
+
+# Data
+
+dl_data <- 
+  tidytuesdayR::tt_load(2022, week = 47)
+museums <- 
+  dl_data$museums
+
+
+# Manipulation ------------------------------------------------------------
+
+
